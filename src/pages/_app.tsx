2ba241c850767ff0170fb894982ad54870826ee6
serkanisyapan <serkanisyapan1@hotmail.com>
2023-06-29 13:55:51 +0300

    setup react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,22 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 import "~/styles/globals.css";
+import { useState } from "react";
+import { AppProps } from "next/app";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
+  const [queryClient] = useState(() => new QueryClient())
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  )
 };
 
 export default MyApp;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,8 @@ export default function Home() {
         <meta name="description" content="Check your stats" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      <main className="bg-gradient-to-b h-screen from-[#2e026d] to-[#15162c]">
+        <h1 className="text-2xl text-white text-center pt-5">HELLO LOL-STATS</h1>
       </main>
     </>
   );
