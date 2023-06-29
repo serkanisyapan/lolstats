dedd6250478944e18078f00f9cd256cc4f1a0a6c
serkanisyapan <serkanisyapan1@hotmail.com>
2023-06-29 13:10:25 +0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>LOL-Stats</title>
+        <meta name="description" content="Check your stats" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className="bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+      </main>
+    </>
+  );
+}
