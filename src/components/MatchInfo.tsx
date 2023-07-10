d34e96ae544996f49e3aedac0022337041797a96
serkanisyapan <serkanisyapan1@hotmail.com>
2023-07-10 13:15:51 +0300

    add dayjs and game played date

--- src/components/MatchInfo.tsx
+++ src/components/MatchInfo.tsx
@@ -2,6 +2,9 @@ import { useQuery } from "@tanstack/react-query"
 import { fetchMatches } from "~/hooks/useMatches"
 import { useUserInfo } from "~/hooks/useUserInfo"
 import { gameModes } from "~/data/gameModes"
+import dayjs from "dayjs"
+import relativeTime from "dayjs/plugin/relativeTime"
+dayjs.extend(relativeTime)
 
 export const MatchInfo = () => {
     const { data: userInfo } = useUserInfo()
@@ -17,9 +20,14 @@ export const MatchInfo = () => {
         <div className="flex flex-col gap-3 mt-7 ml-3 text-white w-full text-xl">
             {matches?.map((match) => {
                 const getGameMode = gameModes[match.info.queueId]
+                const getGameLength = Math.floor(match.info.gameDuration / 60)
+                const getGamePlayedDate = dayjs(match.info.gameCreation).fromNow()
                 return (
                     <div key={match.metadata.matchId} className="p-4 rounded-md bg-[#5D54A1] h-[200px]">
-                        <div>{getGameMode?.type} {getGameMode?.name}</div>
+                        <div className="flex flex-row gap-4 items-center">
+                            <div>{getGameMode?.type} {getGameMode?.name}</div>
+                            <div className="text-sm opacity-70">{getGamePlayedDate} | {getGameLength} min</div>
+                        </div>
                     </div>
                 )
             })}
