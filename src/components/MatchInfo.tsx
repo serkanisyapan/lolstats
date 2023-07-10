7e071b7f90948d42850b8f7f30db97b59088931b
serkanisyapan <serkanisyapan1@hotmail.com>
2023-07-10 21:02:01 +0300

    add counts and kda

--- src/components/MatchInfo.tsx
+++ src/components/MatchInfo.tsx
@@ -22,7 +22,7 @@ export const SummonerSpells = (props: {winCondition: string, spellId: number}) =
 }
 
 export const SummonerItems = (props: {itemId: number}) => {
-    if (props.itemId === 0) return <div className="w-[35px] h-[35px]"></div>
+    if (props.itemId === 0) return <div className="w-[35px] h-[35px] bg-[#433A87] rounded-md"></div>
     return (
         <Image 
             width={35} 
@@ -54,6 +54,7 @@ export const MatchInfo = () => {
                 const getUserMatchData = match.info.participants.find((user) => user.puuid === userInfo.puuid)
                 const checkWinCondition = getUserMatchData?.win ? "border-green-400" : "border-red-500"
                 if (!getUserMatchData) return <div>User does not played any matches yet.</div>
+                const calcSummonerKDA = (getUserMatchData.kills + getUserMatchData.assists) / getUserMatchData.deaths
 
                 return (
                     <div key={match.metadata.matchId} className="p-4 rounded-md bg-[#5D54A1] h-[200px]">
@@ -61,15 +62,29 @@ export const MatchInfo = () => {
                             <div>{getGameMode?.type} {getGameMode?.name}</div>
                             <div className="text-sm opacity-70">{getGamePlayedDate} | {getGameLength} min</div>
                         </div>
-                        <div className="mt-3 flex flex-row gap-4">
+                        <div className="mt-4 flex flex-row gap-4">
                             <div className="relative">
                                 <Image alt={`${getUserMatchData?.championName}'s Image`} width={94} height={94} className={`rounded-full border-4 ${checkWinCondition}`} src={`http://ddragon.leagueoflegends.com/cdn/13.13.1/img/champion/${getUserMatchData?.championName}.png`}/>
                                 <div className={`w-8 h-8 rounded-full absolute right-0 top-16 bg-[#433A87] flex justify-center items-center border ${checkWinCondition}`}>{getUserMatchData?.champLevel}</div>
                             </div>
                             <div>
-                                <SummonerSpells spellId={getUserMatchData.summoner1Id} winCondition={checkWinCondition}/>
-                                <SummonerSpells spellId={getUserMatchData.summoner2Id} winCondition={checkWinCondition}/>
-                                <div className="flex flex-row gap-1 mt-4">
+                                <div className="flex flex-row items-center gap-8">
+                                    <div className="flex flex-row gap-2 items-center">
+                                        <div>
+                                            <SummonerSpells spellId={getUserMatchData.summoner1Id} winCondition={checkWinCondition}/>
+                                            <SummonerSpells spellId={getUserMatchData.summoner2Id} winCondition={checkWinCondition}/>
+                                        </div>
+                                        <div>
+                                            <div className="w-[35px] h-[35px] bg-[#433A87] rounded-md border border-green-400"></div>
+                                            <div className="w-[35px] h-[35px] bg-[#433A87] mt-1 rounded-md border border-green-400"></div>
+                                        </div>
+                                    </div>
+                                    <div className="flex flex-col gap-2 items-center">
+                                        <span>{getUserMatchData.kills} / {getUserMatchData.deaths} / {getUserMatchData.assists}</span>
+                                        <span className="text-sm opacity-70">{calcSummonerKDA.toFixed(1)} KDA</span>
+                                    </div>
+                                </div>
+                                <div className="flex flex-row gap-1 mt-8">
                                     {summonerItems.map((item) => (
                                         // @ts-expect-error
                                         <SummonerItems key={item} itemId={getUserMatchData[item]}/>
